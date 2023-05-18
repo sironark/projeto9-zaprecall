5584aea62e1f64abd2c6c5002a0a643588c93787
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-05-18 11:56:48 -0300

    versão com layout-header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+//import viteLogo from '/vite.svg'
+
+import styled from "styled-components"
+import logo from "./assets/logo.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ContainerJogo>
+       
+        <Header>
+         <img src={logo} alt="logo"/>
+         <p>ZapRecall</p>
+        </Header>
+        
+
+        <Deck>
+          
+        </Deck>
+
+      </ContainerJogo>
     </>
   )
 }
 
 export default App
+
+const ContainerJogo = styled.div`
+  width: 23.4375rem;
+  height: 41.6875rem;
+  background-color: #FB6B6B;
+
+  display: flex;
+  flex-direction: column;
+  padding-top: 2.62500rem;
+  padding-left: 2.3125rem;
+  padding-right: 2.3125rem;
+  box-sizing: border-box;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 5.3125rem;
+  
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  img{
+    width: 3.25rem;
+  };
+  p{
+    font-size: 36px;
+    font-family: 'Righteous';
+    color: white;
+  };
+`;
+
+const Deck = styled.ul`
+
+`;
+
